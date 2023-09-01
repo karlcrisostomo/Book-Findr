4e4593909c3b9a5e8e5e718e15d71a82332c4f13
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-09-01 17:05:57 +0800

    add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,14 +11,15 @@ const Footer = () => {
           <span className=" ml-3 animate-ping bg-black w-2 h-2 block rounded-full"></span>
         </h1>
         <h1 className="flex items-center">
-          Design and Coded by <span className=" ml-1 font-medium">Karl_Crisostomo</span>
+          Design and Coded by{" "}
+          <span className=" ml-1 font-medium">Karl_Crisostomo</span>
           <a
-            href="https://github.com/karlcrisostomo/book-app.git"
+            href="https://github.com/karlcrisostomo/Book-Findr.git"
             target="_blank"
             rel="noopener noreferrer"
           >
             <RxGithubLogo
-              className="cursor-pointer   hover:scale-110 duration-300 ml-2"
+              className="cursor-pointer hover:scale-110 duration-300 ml-2"
               size={24}
             />
           </a>
